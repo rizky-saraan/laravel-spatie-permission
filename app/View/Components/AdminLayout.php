19bd13c5a61f74829f0eeab6bb1f6370a6a048db
saraan <rikisaraan2@gmail.com>
2022-12-11 23:21:32 +0700

    create admin layout

--- app/View/Components/AdminLayout.php
+++ app/View/Components/AdminLayout.php
@@ -4,15 +4,15 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class AppLayout extends Component
+class AdminLayout extends Component
 {
     /**
      * Get the view / contents that represents the component.
      *
-     * @return \Illuminate\View\View
+     * @return \Illuminate\View\View 
      */
     public function render()
     {
-        return view('layouts.app');
+        return view('layouts.admin');
     }
 }
